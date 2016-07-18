b55cda1f24be13b08ce491489565fd0850bb3413
Katarina Tuttle and Maldon Meehan <katarina+maldon@>
2016-07-18 11:14:14 -0700

    Add passing spec tasks for Task method and add front end

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -38,4 +38,23 @@ describe(List) do
     end
   end
 
+  describe('.find') do
+    it "returns a list id " do
+      list1 = List.new({:name => "Epicodus stuff", :id => nil})
+      list1.save()
+      expect(List.find(list1.id())).to(eq(list1))
+    end
+  end
+
+  describe("#tasks") do
+    it("returns an array of tasks for that list") do
+      test_list = List.new({:name => "Epicodus stuff", :id => nil})
+      test_list.save()
+      task1 = Task.new({:description => "Learn SQL", :list_id => test_list.id()})
+      task1.save()
+      task2 = Task.new({:description => "Review Ruby", :list_id => test_list.id()})
+      task2.save()
+      expect(test_list.tasks()).to(eq([task1, task2]))
+    end
+  end
 end
